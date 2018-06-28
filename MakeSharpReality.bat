ce659bf540f41c77317f1a7fa17d99134e306ea9
Peter Vrenken <github.com@petervrenken.nl>
2018-06-28 17:02:03 +0200

    Added CMAKE

--- MakeSharpReality.bat
+++ MakeSharpReality.bat
@@ -20,7 +20,7 @@ if "%VSVER%" == "2017" set "VS_VER=15"
 if "%PLATFORM%" == "x64" (set "TARGET=Visual Studio %VS_VER% Win64") else (set "TARGET=Visual Studio %VS_VER%")
 
 :: Just to ensure that cmake is found - in our case on the TFS build server.
-SET PATH=C:\Program Files (x86)\CMake\bin\;C:\Program Files\CMake\bin\;%PATH%
+SET CMAKE_EXE=tools/cmake/cmake
 
 
 del Urho3D\Urho3D_SharpReality\CMakeCache.txt 2>NUL
@@ -28,13 +28,13 @@ del /S /Q Urho3D\Urho3D_SharpReality\CMakeFiles
 
 cd Urho3D/Source
 
-cmake -E make_directory ../Urho3D_SharpReality
-cmake -E chdir ../Urho3D_SharpReality cmake -G "%TARGET%" ../Urho3D_SharpReality -DURHO3D_D3D11=1 -DUWP=1 -DUWP_HOLO=1 -DURHO3D_NEON=0 -DURHO3D_WIN32_CONSOLE=0 -DURHO3D_NETWORK=1 -DURHO3D_FILEWATCHER=0 -DURHO3D_PROFILING=0 -DURHO3D_THREADING=0 -DURHO3D_PCH=0 -DURHO3D_WEBP=0 -DURHO3D_LUA=0 -DURHO3D_ANGELSCRIPT=0 -VS=%VS_VER% ../../Urho3D/Source/
+%CMAKE_EXE% -E make_directory ../Urho3D_SharpReality
+%CMAKE_EXE% -E chdir ../Urho3D_SharpReality cmake -G "%TARGET%" ../Urho3D_SharpReality -DURHO3D_D3D11=1 -DUWP=1 -DUWP_HOLO=1 -DURHO3D_NEON=0 -DURHO3D_WIN32_CONSOLE=0 -DURHO3D_NETWORK=1 -DURHO3D_FILEWATCHER=0 -DURHO3D_PROFILING=0 -DURHO3D_THREADING=0 -DURHO3D_PCH=0 -DURHO3D_WEBP=0 -DURHO3D_LUA=0 -DURHO3D_ANGELSCRIPT=0 -VS=%VS_VER% ../../Urho3D/Source/
 
 cd ..
 xcopy Urho3D_UWP\MonoUrho.UWP\SdlStub\SDL Urho3D_SharpReality\include\Urho3D\ThirdParty\SDL\* /Y
 cd Urho3D_SharpReality
-cmake --build . --target Urho3D --config %CONFIG%
+%CMAKE_EXE% --build . --target Urho3D --config %CONFIG%
 copy lib\Urho3D%LIB_PREFIX%.lib lib\Urho3D%LIB_PREFIX%_%PLATFORM%.lib /Y
 cd ../..
 
